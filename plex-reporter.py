83ff4b16ee027f2ae898aa086d3e93435f5b11d4
kloptops <kloptops@gmail.com>
2013-07-13 18:47:33 +1000

    Better categorization in plex-reporter.py
    plex-reporter.py
    - Better categorization
    - still messing around with the code.

--- plex-reporter.py
+++ plex-reporter.py
@@ -27,25 +27,50 @@ def event_categorize(event):
     result = []
     # Doesn't work if event starts over new day
     # result.append("{0:04}-{1:02}-{2:02}".format(*event['datetime']))
-    
+    seen = []
+
+    url_collators = (
+        ('/video/:/transcode/segmented', '/video/:/transcode/segmented'),
+        ('/video/:/transcode/universal', '/video/:/transcode/universal'),
+        ('/video/:/transcode/session/', '/video/:/transcode/session/'),
+        )
+
     if 'request_ip' in event:
+        seen.append('ip')
         result.append(event['request_ip'])
 
     if 'url_path' in event:
-        if event['url_path'].startswith('/video/:/transcode/universal'):
-            result.append('/video/:/transcode/universal')
+        seen.append('url')
+        for url_collator_match, url_collator_result in url_collators:
+            if event['url_path'].startswith(url_collator_match):
+                result.append(url_collator_result)
+                break
         else:
             result.append(event['url_path'])
 
+        if (event['url_path'].startswith(
+                '/video/:/transcode/segmented/session') or
+            event['url_path'].startswith(
+                '/video/:/transcode/universal/session')):
+            seen.append('session')
+            result.append(event['url_path'].split('/')[6])
+        elif (event['url_path'].startswith('/video/:/transcode/session/')):
+            seen.append('session')
+            result.append(event['url_path'].split('/')[5])
+
     if 'url_query' in event:
         url_query = event['url_query']
-        if 'ratingKey' in url_query:
-            result.append(url_query['ratingKey'])
-        elif 'key' in url_query:
-            result.append(url_query['key'].rsplit('/', 1)[-1])
-        if 'session' in url_query:
+        if 'session' not in seen and 'session' in url_query:
+            seen.append('session')
             result.append(url_query['session'])
-        elif 'X-Plex-Device-Name' in url_query:
+        if 'session' not in seen and 'ratingKey' in url_query:
+            seen.append('key')
+            result.append(url_query['ratingKey'])
+        elif 'session' not in seen and 'key' in url_query:
+            seen.append('key')
+            result.append(url_query['key'].rsplit('/', 1)[-1])
+        if 'session' not in seen and 'X-Plex-Device-Name' in url_query:
+            seen.append('xpdn')
             result.append(url_query['X-Plex-Device-Name'])
 
     return tuple(result)
@@ -59,9 +84,29 @@ def LogFileLoader(log_file, results=None):
     else:
         open_cmd = open
 
+    paths_not_wanted = (
+        '/:/plugins/',
+        '/library/metadata',
+        '/library/optimize',
+        '/library/section',
+        '/library/onDeck',
+        '/web/',
+        '/system/',
+        )
+
     with open_cmd(log_file, 'rt') as file_handle:
         for line in file_handle:
             line_body = json.loads(line)
+
+            skip = False
+            if 'url_path' in line_body:
+                for path_not_wanted in paths_not_wanted:
+                    if line_body['url_path'].startswith(path_not_wanted):
+                        skip = True
+                        break
+
+            if skip:
+                continue
 
             # For now we are only really interested in categorized events
             line_event = event_categorize(line_body)
@@ -72,6 +117,20 @@ def LogFileLoader(log_file, results=None):
             results.setdefault(line_event, []).append(line_body)
 
     return results
+
+
+
+def parse_event(line_event, line_bodies, file_handle):
+    print_data = [
+        ('10.0.0.4', '/video/:/transcode/universal'),
+        ('10.0.0.6', '/video/:/transcode/segmented', '0C902F73-51D4-4D82-9499-E6234B305CAF'),
+        ('10.0.0.11', '/video/:/transcode/segmented', 'C5B092C3-B261-4344-A09F-A6939E10A468'),
+        ]
+
+    print(line_event, file=file_handle)
+    if line_event in print_data:
+        for line_body in line_bodies:
+            print(json.dumps(line_body, sort_keys=True), file=file_handle)
 
 
 def main():
@@ -104,6 +163,10 @@ def main():
         print("Log - {}".format(log_file))
         LogFileLoader(log_file, all_events)
 
+    with open('output.txt', 'wt', encoding="utf-8") as file_handle:
+        for line_event in sorted(all_events.keys()):
+            all_events[line_event].sort(key=lambda line_body: line_body['datetime'])
+            parse_event(line_event, all_events[line_event], file_handle)
 
 if __name__ == '__main__':
     with LockFile() as lf:
